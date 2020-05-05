3fad140fb7b21977a44790f46d3e2c6e899a10b3
Dawid 'Sloenthran' Nogacz <sloenthran@gmail.com>
2020-05-05 13:23:36 +0200

    Create Doctor class

---
+++ src/main/java/pl/nogacz/rsq/domain/Doctor.java
@@ -0,0 +1,33 @@
+package pl.nogacz.rsq.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity("doctors")
+@Builder
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "surname")
+    private String surname;
+
+    @NotNull
+    @Column(name = "specialization")
+    private String specialization;
+}

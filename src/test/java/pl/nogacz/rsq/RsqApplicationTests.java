e5bdee5e07cd5f92d81ae48c9c336080deb0f06b
Dawid 'Sloenthran' Nogacz <sloenthran@gmail.com>
2020-05-05 13:16:07 +0200

    Init commit

---
+++ src/test/java/pl/nogacz/rsq/RsqApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nogacz.rsq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

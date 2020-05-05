db50f9c07ce734a75d5fcae6f20e2272f1c685be
Dawid 'Sloenthran' Nogacz <sloenthran@gmail.com>
2020-05-05 13:47:32 +0200

    Create Patient class

---
+++ src/main/java/pl/nogacz/rsq/domain/Patient.java
@@ -0,0 +1,33 @@
+package pl.nogacz.rsq.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity(name = "patients")
+@Builder
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @NotNull
+    @Column(name = "surname")
+    private String surname;
+
+    @NotNull
+    @Column(name = "address")
+    private String address;
+}
